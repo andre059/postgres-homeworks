6cc2480791b9696942f865138f90d91c435c0e14
kirillskyeng <117906913+kirillskyeng@users.noreply.github.com>
2023-04-18 13:25:00 +0300

    работает

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,28 @@
 -- SQL-команды для создания таблиц
+
+CREATE TABLE customers
+(
+    customer_id varchar(10) NOT NULL,
+    company_name varchar(40) NOT NULL,
+    contact_name varchar(40) NOT NULL
+);
+
+SELECT * FROM  employees;
+
+CREATE TABLE employees
+(
+	first_name varchar(20) NOT NULL,
+	last_name varchar(20) NOT NULL,
+	title varchar(30) NOT NULL,
+	birth_date varchar(15) NOT NULL,
+	notes text
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(10) NOT NULL,
+	employee_id int NOT NULL,
+	order_date varchar(15) NOT NULL,
+	ship_city varchar(20) NOT NULL
+)
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,30 @@
+import psycopg2
+
+
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+
+# таблица customers
+
+conn = psycopg2.connect(host="localhost", database="north", user="postgres", password="aktiv416384352")
+cur = conn.cursor()
+with open('C:/Users/аа/PycharmProjects/postgres-homeworks/homework-1/north_data/customers_data.csv', 'r') as f:
+    next(f)  # пропустить первую строку (заголовок)
+    cur.copy_from(f, 'customers', sep=',')
+
+# таблица employees
+
+sql = "COPY %s FROM STDIN WITH CSV HEADER DELIMITER AS ','"
+with open('C:/Users/аа/PycharmProjects/postgres-homeworks/homework-1/north_data/employees_data.csv', 'r',
+          encoding="UTF-8") as f:
+    next(f)  # пропустить первую строку (заголовок)
+    cur.execute("truncate " + 'employees' + ";")
+    cur.copy_expert(sql=sql % 'employees', file=f)
+
+# таблица orders
+
+with open('C:/Users/аа/PycharmProjects/postgres-homeworks/homework-1/north_data/orders_data.csv', 'r') as f:
+    next(f)  # пропустить первую строку (заголовок)
+    cur.copy_from(f, 'orders', sep=',')
+
+conn.commit()
+conn.close()
