a5ce4a2fca7bcdd4a9072dbd6fe1ac17fad046f8
andre059 <andreyperm159@mail.ru>
2023-05-26 16:03:40 +0500

    2.4 )

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -58,3 +58,33 @@ CREATE TABLE orders
 );
 
 SELECT * FROM orders
+
+-- вариант 3
+
+CREATE TABLE customers
+(
+	customer_id VARCHAR(10) NOT NULL,
+    company_name VARCHAR(50) NOT NULL,
+    contact_name VARCHAR(50) NOT NULL,
+    PRIMARY KEY (customer_id)
+);
+
+CREATE TABLE employees
+(
+	employee_id INT GENERATED ALWAYS AS IDENTITY,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    birth_date DATE NOT NULL,
+    notes TEXT NOT NULL,
+    PRIMARY KEY (employee_id)
+);
+
+CREATE TABLE orders
+(
+	order_id INT NOT NULL,
+	customer_id VARCHAR(20) NOT NULL REFERENCES customers(customer_id) ON DELETE CASCADE,
+	employee_id INT NOT NULL REFERENCES employees(employee_id) ON DELETE CASCADE,
+	order_date DATE NOT NULL,
+	ship_city varchar(20) NOT NULL
+);
